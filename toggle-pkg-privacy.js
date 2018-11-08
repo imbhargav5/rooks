93068474570b1c4aaa757538dab83a6a9d5e2de3
Bhargav Ponnapalli <bhargavponnapalli.5@gmail.com>
2018-11-08 21:25:28 +0530

    Add util

---
+++ toggle-pkg-privacy.js
@@ -0,0 +1,48 @@
+const meow = require("meow");
+const readPkgUp = require("read-pkg-up");
+const writePkg = require("write-pkg");
+
+const cli = meow(
+  `
+    Usage
+      $ cd directoryOfPkg
+      $ directoryOfPkg > toggle-pkg-privacy --private
+      $ directoryOfPkg > toggle-pkg-privacy --public
+ 
+    Options
+      --public, - Set pkg json public
+      --private, -  Set pkg json private
+ 
+    Examples
+      $ directoryOfPkg > toggle-pkg-privacy --public
+      $ directoryOfPkg > toggle-pkg-privacy --private
+`,
+  {
+    flags: {
+      public: {
+        type: "boolean",
+        alias: "p"
+      },
+      private: {
+        type: "boolean",
+        alias: "pr"
+      }
+    }
+  }
+);
+
+if (cli.public && cli.private) {
+  throw new Error("Use either private or public flags. Not both");
+}
+
+const result = readPkgUp.sync();
+if (result && result.pkg) {
+  console.log(result.pkg);
+  if (cli.public) {
+    result.pkg.private = false;
+  }
+  if (cli.private) {
+    result.pkg.public = false;
+  }
+  writePkg(result.pkg);
+}
