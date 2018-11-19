4f4f1f604e58bbf59b9c2d68d81efde2aa481972
Bhargav Ponnapalli <bhargavponnapalli.5@gmail.com>
2018-11-19 11:14:35 +0530

    Add mutation observer src

--- packages/mutation-observer/src/index.js
+++ packages/mutation-observer/src/index.js
@@ -1,1 +1,28 @@
-module.exports=true;
+import { useEffect } from "react";
+
+var config = {
+  attributes: true,
+  characterData: true,
+  subtree: true,
+  childList: true
+};
+
+export default function(ref, callback, options = config) {
+  useEffect(
+    () => {
+      // Create an observer instance linked to the callback function
+      if (ref.current) {
+        const observer = new MutationObserver((mutationList, observer) => {
+          callback(mutationList, observer);
+        });
+
+        // Start observing the target node for configured mutations
+        observer.observe(ref.current, options);
+        return () => {
+          observer.disconnect();
+        };
+      }
+    },
+    [ref.current, callback, options]
+  );
+}
