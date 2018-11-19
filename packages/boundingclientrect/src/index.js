bda0e5b8f08e605f9ecf54f053a1d4a580436a48
Bhargav Ponnapalli <bhargavponnapalli.5@gmail.com>
2018-11-19 11:12:29 +0530

    Add boundingclientrect

--- packages/boundingclientrect/src/index.js
+++ packages/boundingclientrect/src/index.js
@@ -1,1 +1,52 @@
-module.exports=true;
+import { useState, useEffect } from "react";
+
+var config = {
+  attributes: true,
+  characterData: true,
+  subtree: true,
+  childList: true
+};
+
+export default function(ref) {
+  function getBoundingClientRect() {
+    if (ref.current) {
+      return ref.current.getBoundingClientRect();
+    }
+    return null;
+  }
+
+  const [value, setValue] = useState(null);
+
+  function update() {
+    setValue(getBoundingClientRect());
+  }
+
+  useEffect(
+    () => {
+      update();
+    },
+    [ref.current]
+  );
+
+  function mutate(mutationList) {
+    update();
+  }
+
+  useEffect(
+    () => {
+      // Create an observer instance linked to the callback function
+      if (ref.current) {
+        const observer = new MutationObserver(mutate);
+
+        // Start observing the target node for configured mutations
+        observer.observe(ref.current, config);
+        return () => {
+          observer.disconnect();
+        };
+      }
+    },
+    [ref.current]
+  );
+
+  return value;
+}
