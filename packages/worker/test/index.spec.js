f4ef40a85f2b9df6248ad393fc56871a036e864a
Bhargav Ponnapalli <bhargavponnapalli.5@gmail.com>
2018-11-22 00:07:31 +0530

    Update dev setup

--- packages/worker/test/index.spec.js
+++ packages/worker/test/index.spec.js
@@ -1,1 +1,13 @@
-module.exports=true;
+/**
+ * @jest-environment jsdom
+ */
+import React from "react";
+import useWorker from "../src";
+
+describe("useWorker", () => {
+  it("should be defined", () => {
+    expect(useWorker).toBeDefined();
+  });
+});
+
+// figure out tests
