df126632cf99583724edc1d0b20dce65dffe8c37
Bhargav Ponnapalli <bhargavponnapalli.5@gmail.com>
2018-11-22 18:41:12 +0530

    Readme fail fallback

--- packages/website/actions/getReadme.js
+++ packages/website/actions/getReadme.js
@@ -1,7 +1,12 @@
 import fetch from "isomorphic-fetch";
 export default async function(dirName) {
-  const response = await fetch(
+  let response = await fetch(
     `https://raw.githubusercontent.com/react-hooks-org/rooks/master/packages/${dirName}/README.md`
   );
+  if (!response.ok) {
+    response = await fetch(
+      `https://raw.githubusercontent.com/react-hooks-org/rooks/master/packages/${dirName}/readme.md`
+    );
+  }
   return await response.text();
 }
