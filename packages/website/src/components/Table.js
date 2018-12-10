7a37add0e99f6ac2652480e0f2e27bc57814b1a1
Bhargav Ponnapalli <bhargavponnapalli.5@gmail.com>
2018-12-10 21:57:05 +0530

    Table with borders

--- packages/website/src/components/Table.js
+++ packages/website/src/components/Table.js
@@ -9,11 +9,17 @@ const Table = styled.table`
     display: table-header-group;
     vertical-align: middle;
     border-color: inherit;
+    tr {
+      background-color: #fafafa;
+    }
   }
   tbody {
     display: table-row-group;
     vertical-align: middle;
     border-color: inherit;
+    tr:nth-child(even) {
+      background-color: #fafafa;
+    }
   }
   tr {
     display: table-row;
@@ -23,13 +29,13 @@ const Table = styled.table`
   th,
   td {
     border: 1px solid #dbdbdb;
-    border-width: 0 0 1px;
+    border-width: 1px;
     padding: 0.5em 0.75em;
     vertical-align: top;
   }
   thead th {
     border-width: 0 0 2px;
-    border-width: 0 0 1px;
+    border-width: 1px;
     padding: 0.5em 0.75em;
     vertical-align: top;
     text-align: left;
