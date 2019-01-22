51b8c1004d6d8feaf98603d753b10da69ac5aa1a
Bhargav Ponnapalli <bhargavponnapalli.5@gmail.com>
2019-01-22 15:23:09 +0530

    Add test cases

--- packages/did-mount/test/index.spec.js
+++ packages/did-mount/test/index.spec.js
@@ -1,10 +1,41 @@
 /**
  * @jest-environment jsdom
  */
-import React from "react";
+import React, { useState } from "react";
 import useDidMount from "../src";
+import { render, cleanup } from "react-testing-library";
 
 describe("useDidMount", () => {
+  let App;
+  beforeEach(() => {
+    App = function() {
+      const [value, setValue] = useState(0);
+      useDidMount(() => {
+        setValue(1);
+      });
+      return (
+        <div>
+          <span data-testid="element">{value}</span>
+        </div>
+      );
+    };
+  });
+  afterEach(cleanup); // <-- add this
+
+  it("initializes correctly", () => {
+    const { getByTestId, rerender } = render(<App />);
+    const renderedElement = getByTestId("element");
+    expect(parseInt(renderedElement.textContent)).toEqual(0);
+  });
+
+  it("gets called on mount correctly", () => {
+    const { getByTestId, rerender } = render(<App />);
+    const renderedElement = getByTestId("element");
+    expect(parseInt(renderedElement.textContent)).toEqual(0);
+    rerender();
+    expect(parseInt(renderedElement.textContent)).toEqual(1);
+  });
+
   it("should be defined", () => {
     expect(useDidMount).toBeDefined();
   });
