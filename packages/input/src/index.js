af05128f306ef56e2634322c4e565992149ba1ca
Bhargav Ponnapalli <bhargavponnapalli.5@gmail.com>
2019-01-23 15:53:09 +0530

    Add test cases

--- packages/input/src/index.js
+++ packages/input/src/index.js
@@ -1,8 +1,29 @@
 import { useState, useEffect } from "react";
+/**
+ *
+ * @typedef handler
+ * @type {Object}
+ * @property {string} value The value of the input
+ * @property {function}  onChange Change handler
+ */
+
+/**
+ *
+ * @typedef options
+ * @type {Object}
+ * @property {function} validate Validator function which can be used to prevent updates
+ */
 
 const defaultOptions = {};
 
-export default function useInput(initialValue = "", opts = defaultOptions) {
+/**
+ *
+ * Input Hook
+ * @param {string} [initialValue=""] Initial value of the input
+ * @param {options} [opts={}] Options object
+ * @returns {handler}
+ */
+function useInput(initialValue = "", opts = defaultOptions) {
   const [value, setValue] = useState(initialValue);
   function onChange(e) {
     const newValue = e.target.value;
@@ -22,15 +43,7 @@ export default function useInput(initialValue = "", opts = defaultOptions) {
     [initialValue]
   );
 
-  // // run it when updates
-  // useEffect(
-  //   () => {
-  //     if (typeof opts.onChange === "function") {
-  //       opts.onChange(value);
-  //     }
-  //   },
-  //   [value]
-  // );
-
   return { value, onChange };
 }
+
+module.exports = useInput;
--- packages/input/test/index.spec.js
+++ packages/input/test/index.spec.js
@@ -3,10 +3,132 @@
  */
 import React from "react";
 import useInput from "../src";
+import {
+  render,
+  cleanup,
+  fireEvent,
+  flushEffects
+} from "react-testing-library";
 
 describe("useInput", () => {
-  it("should be defined", () => {
-    expect(useInput).toBeDefined();
+  // basic tests
+  describe("basic", () => {
+    let App;
+    beforeEach(() => {
+      App = function() {
+        const myInput = useInput("hello");
+        return (
+          <div>
+            <input data-testid="input-element" {...myInput} />
+            <input data-testid="display-element" {...myInput} />
+          </div>
+        );
+      };
+    });
+    afterEach(cleanup); // <-- add this
+
+    it("should be defined", () => {
+      expect(useInput).toBeDefined();
+    });
+    it("sets initial value correctly", () => {
+      const { getByTestId, rerender } = render(<App />);
+      const inputElement = getByTestId("input-element");
+      expect(inputElement.value).toBe("hello");
+    });
+    it("updates value correctly", () => {
+      const { getByTestId, rerender } = render(<App />);
+      const inputElement = getByTestId("input-element");
+      const displayElement = getByTestId("display-element");
+      expect(inputElement.value).toBe("hello");
+      expect(displayElement.value).toBe("hello");
+      fireEvent.change(inputElement, {
+        target: {
+          value: "world"
+        }
+      });
+      expect(inputElement.value).toBe("world");
+      expect(displayElement.value).toBe("world");
+    });
+  });
+  // validate
+  describe("validate", () => {
+    let App;
+    beforeEach(() => {
+      App = function() {
+        const myInput = useInput(5, {
+          validate: newValue => newValue < 10
+        });
+        return (
+          <div>
+            <input type="number" data-testid="input-element" {...myInput} />
+          </div>
+        );
+      };
+    });
+    afterEach(cleanup); // <-- add this
+
+    it("does not update if validate returns false", () => {
+      const { getByTestId, rerender } = render(<App />);
+      const inputElement = getByTestId("input-element");
+      fireEvent.change(inputElement, {
+        target: {
+          value: 10
+        }
+      });
+      expect(parseInt(inputElement.value)).toBe(5);
+    });
+    it("updates if validate returns true", () => {
+      const { getByTestId, rerender } = render(<App />);
+      const inputElement = getByTestId("input-element");
+      fireEvent.change(inputElement, {
+        target: {
+          value: 9
+        }
+      });
+      expect(parseInt(inputElement.value)).toBe(9);
+    });
+  });
+
+  describe("multiple", () => {
+    let App;
+    beforeEach(() => {
+      App = function() {
+        const myInput = useInput(5);
+        const myInput2 = useInput(myInput.value);
+        return (
+          <div>
+            <input type="number" data-testid="input-element1" {...myInput} />
+            <input type="number" data-testid="input-element2" {...myInput2} />
+          </div>
+        );
+      };
+    });
+    afterEach(cleanup); // <-- add this
+
+    it("updates value of input if initial value changes ", () => {
+      const { getByTestId, rerender } = render(<App />);
+      const inputElement1 = getByTestId("input-element1");
+      const inputElement2 = getByTestId("input-element2");
+      expect(parseInt(inputElement1.value)).toBe(5);
+      expect(parseInt(inputElement2.value)).toBe(5);
+
+      fireEvent.change(inputElement2, {
+        target: {
+          value: 6
+        }
+      });
+      expect(parseInt(inputElement2.value)).toBe(6);
+
+      fireEvent.change(inputElement1, {
+        target: {
+          value: 10
+        }
+      });
+
+      expect(parseInt(inputElement1.value)).toBe(10);
+      flushEffects();
+      expect(parseInt(inputElement2.value)).toBe(10);
+    });
   });
 });
 
