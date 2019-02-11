d697b9a2448f57b34518d2dfb645d91b14013ad3
Wei Zhu <yesmeck@gmail.com>
2019-02-11 18:00:38 +0800

    Change keys of Options to optional.

--- packages/visibility-sensor/index.d.ts
+++ packages/visibility-sensor/index.d.ts
@@ -1,17 +1,17 @@
 import { Ref } from "react";
 
 type Options = {
-  intervalCheck: boolean;
-  partialVisibility: boolean;
-  containment: HTMLElement;
-  scrollCheck: boolean;
-  scrollDebounce: number;
-  scrollThrottle: number;
-  resizeCheck: boolean;
-  resizeDebounce: number;
-  resizeThrottle: number;
-  shouldCheckOnMount: boolean;
-  minTopValue: number;
+  intervalCheck?: boolean;
+  partialVisibility?: boolean;
+  containment?: HTMLElement;
+  scrollCheck?: boolean;
+  scrollDebounce?: number;
+  scrollThrottle?: number;
+  resizeCheck?: boolean;
+  resizeDebounce?: number;
+  resizeThrottle?: number;
+  shouldCheckOnMount?: boolean;
+  minTopValue?: number;
 };
 
 type VisibilityRect = {
