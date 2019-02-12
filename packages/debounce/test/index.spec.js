c077f36d01fc9e1c1654076952fa46416df619e5
Brian Steere <dianoga7@3dgo.net>
2019-02-12 16:45:52 -0600

    Add debounce hook

---
+++ packages/debounce/test/index.spec.js
@@ -0,0 +1,13 @@
+/**
+ * @jest-environment jsdom
+ */
+import React from "react";
+import useDebounce from "..";
+
+describe("useDebounce", () => {
+  it("should be defined", () => {
+    expect(useDebounce).toBeDefined();
+  });
+});
+
+// figure out tests
