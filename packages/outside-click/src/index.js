ec6a22631a4caa18399570881d7a61d458657278
Bhargav Ponnapalli <bhargavponnapalli.5@gmail.com>
2019-02-22 10:12:20 +0530

    Make outside-click server renderable

--- packages/outside-click/src/index.js
+++ packages/outside-click/src/index.js
@@ -1,9 +1,6 @@
 import { useLayoutEffect } from "react";
 
 function useOutsideClick(ref, handler, when = true) {
-  if ('ontouchstart' in document.documentElement) {
-    document.body.style.cursor = 'pointer'
-  }
   function handle(e) {
     if (ref && ref.current && !ref.current.contains(e.target)) {
       handler(e);
@@ -12,8 +9,10 @@ function useOutsideClick(ref, handler, when = true) {
   useLayoutEffect(() => {
     if (when) {
       document.addEventListener("click", handle);
+      document.addEventListener("ontouchstart", handle);
       return () => {
         document.removeEventListener("click", handle);
+        document.removeEventListener("ontouchend", handle);
       };
     }
   }, [ref, handler, when]);
