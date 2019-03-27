6a861285e960cf50bb746f1136cbb8e4e64c417b
Christian Rackerseder <git@echooff.de>
2019-03-27 13:10:44 +0100

    📖 Add JSDoc documentation

--- packages/timeout/index.d.ts
+++ packages/timeout/index.d.ts
@@ -1,8 +1,19 @@
 interface Timeout {
+  /**
+   * Clear the timeout
+   */
   clear(): void;
+  /**
+   * Start the timeout
+   */
   start(): void;
 }
 
+/**
+ *
+ * @param callback Function to be executed in timeout
+ * @param {number} [timeoutDelay=0] Number in milliseconds after which callback is to be run. Default is 0.
+ */
 export default function useTimeout(
   callback: () => void,
   timeoutDelay?: number
