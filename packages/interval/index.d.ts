aa0212682a70b37897ccc605650f941b4d664e01
Christian Rackerseder <git@echooff.de>
2019-03-27 13:05:58 +0100

    📖 Add JSDoc documentation

--- packages/interval/index.d.ts
+++ packages/interval/index.d.ts
@@ -1,9 +1,24 @@
 interface Interval {
+  /**
+   * Start the interval
+   */
   start(): void;
+  /**
+   * Stop the interval
+   */
   stop(): void;
+  /**
+   * IntervalId of the interval
+   */
   intervalId: number;
 }
 
+/**
+ *
+ * @param callback Function be invoked after each interval duration
+ * @param {number} intervalDuration Duration in milliseconds after which callback is invoked
+ * @param {boolean} [startImmediate=false] Should the timer start immediately or not. Default is false.
+ */
 export default function useInterval(
   callback: () => void,
   intervalDuration: number,
