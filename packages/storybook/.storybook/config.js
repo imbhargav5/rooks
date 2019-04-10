c8d78ec1c979c5884880d3bcb705526afa0737a4
Bhargav Ponnapalli <bhargavponnapalli.5@gmail.com>
2019-04-10 19:37:58 +0530

    Update storybook config

--- packages/storybook/.storybook/config.js
+++ packages/storybook/.storybook/config.js
@@ -1,4 +1,77 @@
-import { configure } from "@storybook/react";
+import { addParameters, configure } from "@storybook/react";
+import theme from "./yourTheme";
+
+// Option defaults:
+addParameters({
+  options: {
+    theme,
+    /**
+     * show story component as full screen
+     * @type {Boolean}
+     */
+    isFullScreen: false,
+    /**
+     * display panel that shows a list of stories
+     * @type {Boolean}
+     */
+    showNav: true,
+    /**
+     * display panel that shows addon configurations
+     * @type {Boolean}
+     */
+    showPanel: true,
+    /**
+     * where to show the addon panel
+     * @type {('bottom'|'right')}
+     */
+    panelPosition: "right",
+    /**
+     * sorts stories
+     * @type {Boolean}
+     */
+    sortStoriesByKind: false,
+    /**
+     * regex for finding the hierarchy separator
+     * @example:
+     *   null - turn off hierarchy
+     *   /\// - split by `/`
+     *   /\./ - split by `.`
+     *   /\/|\./ - split by `/` or `.`
+     * @type {Regex}
+     */
+    hierarchySeparator: /\/|\./,
+    /**
+     * regex for finding the hierarchy root separator
+     * @example:
+     *   null - turn off multiple hierarchy roots
+     *   /\|/ - split by `|`
+     * @type {Regex}
+     */
+    hierarchyRootSeparator: /\|/,
+    /**
+     * sidebar tree animations
+     * @type {Boolean}
+     */
+    sidebarAnimations: true,
+    /**
+     * enable/disable shortcuts
+     * @type {Boolean}
+     */
+    enableShortcuts: true,
+    /**
+     * show/hide tool bar
+     * @type {Boolean}
+     */
+    isToolshown: true
+  }
+});
+
+// addDecorator(
+//   withOptions({
+//     name: "Blitz",
+//     url: "https://github.com/solomidnet/blitz-ui"
+//   })
+// );
 
 function loadStories() {
   require("../src/index.js");
---
+++ packages/storybook/.storybook/yourTheme.js
@@ -0,0 +1,37 @@
+import { create } from "@storybook/theming";
+
+export default create({
+  base: "light",
+
+  // colorPrimary: "hotpink",
+  // colorSecondary: "deepskyblue",
+
+  // // UI
+  appBg: "white",
+  // appContentBg: "skyblue",
+  // appBorderColor: "grey",
+  // appBorderRadius: 4,
+
+  // // Typography
+  // fontBase: '"Open Sans", sans-serif',
+  // fontCode: "monospace",
+
+  // // Text colors
+  // textColor: "black",
+  // textInverseColor: "rgba(255,255,255,0.9)",
+
+  // // Toolbar default and active colors
+  // barTextColor: "silver",
+  // barSelectedColor: "black",
+  // barBg: "hotpink",
+
+  // // Form colors
+  // inputBg: "white",
+  // inputBorder: "silver",
+  // inputTextColor: "black",
+  // inputBorderRadius: 4,
+
+  brandTitle: "Rooks storybook",
+  brandUrl: "https://react-hooks.com",
+  brandImage: "https://i.gyazo.com/67b004be5aa811e9ccd8375b9ce274e1.png"
+});
