a1711031bc3201b9ee56a31b0c8172be3772636e
Bhargav Ponnapalli <bhargavponnapalli.5@gmail.com>
2019-06-09 14:09:39 +0530

    Update storybook example

--- packages/storybook/src/debounce.js
+++ packages/storybook/src/debounce.js
@@ -18,10 +18,41 @@ function DebouncedDemo() {
   );
 }
 
+function DebouncedDemoEvents() {
+  const [value, setValue] = useState("");
+  const [autocompleteValue, setAutocompleteValue] = useState("");
+
+  const onChange = e => {
+    setValue(e.target.value);
+    updateAutocomplete(e);
+  };
+  const updateAutocomplete = useDebounce(e => {
+    setAutocompleteValue(e.target.value);
+  }, 500);
+
+  return (
+    <div>
+      <form>
+        <h1>Type value and watch the second value change with debounce!</h1>
+        <fieldset>
+          <label htmlFor="value">Typed value</label>
+          <input id="value" value={value} onChange={onChange} />
+        </fieldset>
+        <fieldset>
+          <p>This value is a debounced version of typed value</p>
+          <label htmlFor="autocomplete">Value used for autocomplete.</label>
+          <input id="autocomplete" disabled value={autocompleteValue} />
+        </fieldset>
+      </form>
+    </div>
+  );
+}
+
 storiesOf("useDebounce", module)
   .addParameters({
     readme: {
       sidebar: README
     }
   })
-  .add("basic example", () => <DebouncedDemo />);
+  .add("basic example", () => <DebouncedDemo />)
+  .add("events example", () => <DebouncedDemoEvents />);
