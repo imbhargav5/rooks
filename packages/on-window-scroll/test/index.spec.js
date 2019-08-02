ce1d92117ae91876ea0c94a6b0923b2f4868709b
Bhargav Ponnapalli <bhargavponnapalli.5@gmail.com>
2019-08-02 16:38:25 +0530

    Add tests for resize and scroll hooks

--- packages/on-window-scroll/test/index.spec.js
+++ packages/on-window-scroll/test/index.spec.js
@@ -3,10 +3,31 @@
  */
 import React from "react";
 import useOnWindowScroll from "..";
+import { render, cleanup, fireEvent } from "@testing-library/react";
 
 describe("useOnWindowScroll", () => {
   it("should be defined", () => {
     expect(useOnWindowScroll).toBeDefined();
+  });
+
+  describe("basic", () => {
+    let App;
+    const mockCallback = jest.fn(() => {});
+    beforeEach(() => {
+      App = function() {
+        useOnWindowScroll(mockCallback);
+        return null;
+      };
+    });
+    afterEach(cleanup); // <-- add this
+    it("should call callback after scroll", () => {
+      render(<App />);
+      fireEvent(window, new Event("scroll"));
+      expect(mockCallback.mock.calls.length).toBe(1);
+      fireEvent(window, new Event("scroll"));
+      fireEvent(window, new Event("scroll"));
+      expect(mockCallback.mock.calls.length).toBe(3);
+    });
   });
 });
 
