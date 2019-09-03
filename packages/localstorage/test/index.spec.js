6f99ff99604931317abdbe23043b6e80b9eecacc
Bhargav Ponnapalli <bhargavponnapalli.5@gmail.com>
2019-09-03 11:13:06 +0530

    Add basic tests for localstorage and session storage

--- packages/localstorage/test/index.spec.js
+++ packages/localstorage/test/index.spec.js
@@ -3,10 +3,63 @@
  */
 import React from "react";
 import useLocalstorage from "..";
+import { render, cleanup, getByTestId } from "@testing-library/react";
 
-describe("useLocalstorage", () => {
+describe("useLocalStorage defined", () => {
   it("should be defined", () => {
     expect(useLocalstorage).toBeDefined();
+  });
+});
+
+describe("useLocalstorage with object destructuring", () => {
+  let App;
+  // let firstCallback
+  beforeEach(() => {
+    // firstCallback = jest.fn()
+    App = function() {
+      const { value } = useLocalstorage("test-value", "hello");
+
+      return (
+        <div data-testid="container">
+          <p data-testid="value">{value}</p>
+        </div>
+      );
+    };
+    //end
+  });
+
+  afterEach(cleanup);
+
+  it("it initializes correctly", () => {
+    const { container } = render(<App />);
+    const valueElement = getByTestId(container, "value");
+    expect(valueElement.innerHTML).toBe("hello");
+  });
+});
+
+describe("useLocalstorage with array destructuring", () => {
+  let App;
+  // let firstCallback
+  beforeEach(() => {
+    // firstCallback = jest.fn()
+    App = function() {
+      const [currentValue] = useLocalstorage("test-value", "hello");
+
+      return (
+        <div data-testid="container">
+          <p data-testid="value">{currentValue}</p>
+        </div>
+      );
+    };
+    //end
+  });
+
+  afterEach(cleanup);
+
+  it("it initializes correctly", () => {
+    const { container } = render(<App />);
+    const valueElement = getByTestId(container, "value");
+    expect(valueElement.innerHTML).toBe("hello");
   });
 });
 
--- packages/sessionstorage/test/index.spec.js
+++ packages/sessionstorage/test/index.spec.js
@@ -3,7 +3,71 @@
  */
 import React from "react";
 import useSessionstorage from "..";
-import { render, cleanup, act } from "@testing-library/react";
+import { render, cleanup, getByTestId } from "@testing-library/react";
+
+/**
+ * @jest-environment jsdom
+ */
+
+describe("useSessionstorage defined", () => {
+  it("should be defined", () => {
+    expect(useSessionstorage).toBeDefined();
+  });
+});
+
+describe("useSessionstorage with object destructuring", () => {
+  let App;
+  // let firstCallback
+  beforeEach(() => {
+    // firstCallback = jest.fn()
+    App = function() {
+      const { value } = useSessionstorage("test-value", "hello");
+
+      return (
+        <div data-testid="container">
+          <p data-testid="value">{value}</p>
+        </div>
+      );
+    };
+    //end
+  });
+
+  afterEach(cleanup);
+
+  it("it initializes correctly", () => {
+    const { container } = render(<App />);
+    const valueElement = getByTestId(container, "value");
+    expect(valueElement.innerHTML).toBe("hello");
+  });
+});
+
+describe("useSessionstorage with array destructuring", () => {
+  let App;
+  // let firstCallback
+  beforeEach(() => {
+    // firstCallback = jest.fn()
+    App = function() {
+      const [currentValue] = useSessionstorage("test-value", "hello");
+
+      return (
+        <div data-testid="container">
+          <p data-testid="value">{currentValue}</p>
+        </div>
+      );
+    };
+    //end
+  });
+
+  afterEach(cleanup);
+
+  it("it initializes correctly", () => {
+    const { container } = render(<App />);
+    const valueElement = getByTestId(container, "value");
+    expect(valueElement.innerHTML).toBe("hello");
+  });
+});
+
+// figure out tests
 
 describe("useSessionstorage", () => {
   let App;
