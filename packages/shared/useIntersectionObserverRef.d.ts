32558c82e83e9778ecdbd0ea9b907d45793f3f6d
Bhargav Ponnapalli <bhargavponnapalli.5@gmail.com>
2019-10-02 23:34:13 +0530

    useIntersectionObserverRef

---
+++ packages/shared/useIntersectionObserverRef.d.ts
@@ -0,0 +1,12 @@
+import { HTMLElementOrNull } from "./utils";
+/**
+ *
+ * useIntersectionObserverRef hook
+ *
+ * Returns a mutation observer for a React Ref and fires a callback
+ *
+ * @param {IntersectionObserverCallback} callback Function that needs to be fired on mutation
+ * @param {IntersectionObserverInit} options
+ */
+declare function useIntersectionObserverRef(callback: IntersectionObserverCallback, options?: IntersectionObserverInit): ((node: HTMLElementOrNull) => void)[];
+export { useIntersectionObserverRef };
