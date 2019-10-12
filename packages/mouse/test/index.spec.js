1a24b62f4e5c5aa0e583ad8498d80cc2bed51764
Braxton M. Christensen <braxton.christensen@gmail.com>
2019-10-12 10:56:52 -0600

    fix(mouse): clean up white space
    remove white space in tests

--- packages/mouse/test/index.spec.js
+++ packages/mouse/test/index.spec.js
@@ -7,12 +7,12 @@ import useMouse from "..";
 
 const TestMouse = () => {
   const mouse = useMouse();
-
   return JSON.stringify(mouse);
 };
 
 describe("useMouse", () => {
   afterEach(cleanup);
+
   it("should be defined", () => {
     expect(useMouse).toBeDefined();
   });
@@ -21,7 +21,6 @@ describe("useMouse", () => {
     const { queryByText } = render(<TestMouse />);
     const expected =
       '{"x":null,"y":null,"screenX":null,"screenY":null,"pageX":null,"pageY":null,"clientX":null,"clientY":null,"movementX":null,"movementY":null,"offsetX":null,"offsetY":null}';
-
     expect(queryByText(expected)).toBeTruthy();
   });
 
@@ -33,9 +32,7 @@ describe("useMouse", () => {
     );
     const expected =
       '{"screenX":0,"screenY":0,"clientX":80,"clientY":20,"x":0,"y":0}';
-
     fireEvent.mouseMove(document, { clientX: 80, clientY: 20 });
-
     expect(queryByText(expected)).toBeTruthy();
   });
 
@@ -47,9 +44,7 @@ describe("useMouse", () => {
     );
     const expected =
       '{"screenX":80,"screenY":20,"clientX":0,"clientY":0,"x":80,"y":20}';
-
     fireEvent.mouseMove(document, { screenX: 80, screenY: 20 });
-
     expect(queryByText(expected)).toBeTruthy();
   });
 });
