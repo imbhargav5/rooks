7b09d474e20359ca349172416ffc54ffde3dcc53
Bhargav Ponnapalli <bhargavponnapalli.5@gmail.com>
2019-12-08 22:33:39 +0530

    Add another action

---
+++ .github/actions/get-release-tags/main.js
@@ -0,0 +1,18 @@
+const cp = require("child_process");
+cp.execSync(`cd ${__dirname}; npm ci`);
+
+const core = require("@actions/core");
+
+const exec = cmd =>
+  cp
+    .execSync(cmd)
+    .toString()
+    .trim();
+
+const currentTag = exec(
+  `git describe --abbrev=0 --tags ${process.env.GITHUB_SHA}`
+);
+const lastTag = exec(`git describe --abbrev=0 --tags ${currentTag}^`);
+
+core.setOutput("old", lastTag);
+core.setOutput("new", currentTag);
