13e521a4b74999447b7258d71790029b62d278ce
Bhargav Ponnapalli <bhargavponnapalli.5@gmail.com>
2019-12-17 17:13:47 +0530

    Update gitignore

--- helpers/docs.js
+++ helpers/docs.js
@@ -1,14 +1,11 @@
 const packageName = process.env.LERNA_PACKAGE_NAME;
 const newReadmeFileName = packageName.startsWith('@rooks') ? packageName.split('@rooks/')[1] : packageName;
 
-const util = require('util');
 const writeFileSync = require('fs').writeFileSync;
-const readFile = util.promisify(require('fs').readFile);
-const writeFile = util.promisify(require('fs').writeFile);
-const exec = util.promisify(require('child_process').exec);
+const readFileSync = require('fs').readFileSync;
 
-async function ls() {
-    const fileContent = await readFile(`./README.md`, 'utf8');
+function ls() {
+    const fileContent = readFileSync(`./README.md`, 'utf8');
     const updatedFileContent = `---
 id: ${newReadmeFileName}
 title: ${newReadmeFileName}
@@ -17,31 +14,48 @@ sidebar_label: ${newReadmeFileName}
 
 ${fileContent}
     `;
-    await writeFile(`../docusaurus/docs/${newReadmeFileName}.md`, updatedFileContent, 'utf8');
+    writeFileSync(`../docusaurus/docs/${newReadmeFileName}.md`, updatedFileContent, 'utf8');
 }
 
 
-ls();
 
-async function addToSidebarJson() {
+
+function addToSidebarJson() {
     if (newReadmeFileName === 'rooks') {
         return;
     }
-    const currentSidebarJson = JSON.parse(await readFile(`../docusaurus/website/sidebars.json`, 'utf8'));
-    console.log(currentSidebarJson)
-
-    if (Object.keys(currentSidebarJson.docs['Independent Packages']).includes('newReadmeFileName')) {
-        return;
-    }
-    const independentPackages = [...currentSidebarJson.docs["Independent Packages"], newReadmeFileName].sort();
-    const newSidebarJson = {
-        ...currentSidebarJson,
-        docs: {
-            ...currentSidebarJson.docs,
-            ["Independent Packages"]: independentPackages
+    let currentSidebarJson
+    let fileContent
+    try {
+        fileContent = readFileSync(`../docusaurus/website/sidebars.json`, 'utf8');
+        if (!fileContent || fileContent === '') {
+            console.log({ newReadmeFileName })
         }
+        currentSidebarJson = JSON.parse(fileContent);
+        if (Object.keys(currentSidebarJson.docs['Independent Packages']).includes('newReadmeFileName')) {
+            return;
+        }
+        const independentPackages = Array.from(new Set([...currentSidebarJson.docs["Independent Packages"], newReadmeFileName].sort()));
+        const newSidebarJson = {
+            ...currentSidebarJson,
+            docs: {
+                ...currentSidebarJson.docs,
+                ["Independent Packages"]: independentPackages
+            }
+        }
+        writeFileSync(`../docusaurus/website/sidebars.json`, JSON.stringify(newSidebarJson, null, 2), 'utf-8');
+    } catch (err) {
+        console.log("----")
+        console.log(err);
+        console.log("----")
     }
-    writeFileSync(`../docusaurus/website/sidebars.json`, JSON.stringify(newSidebarJson, null, 2));
+
+
 }
 
-addToSidebarJson();
+try {
+    ls();
+    addToSidebarJson();
+} catch (err) {
+    console.log(err)
+}
