97502155f8b4879d7b5754b02eb7a847c7b09353
Daniel Holmes <daniel@danielholmes.org>
2020-08-16 10:12:46 +1000

    Fixed useFullscreen TypeScript definition

--- packages/fullscreen/index.d.ts
+++ packages/fullscreen/index.d.ts
@@ -1,1 +1,14 @@
-export default function useFullscreen(): void;
+type EventCallback = (this: Document, ev: any) => any;
+
+interface FullscreenApi {
+  readonly isEnabled: boolean;
+  readonly toggle: (element?: HTMLElement) => Promise<unknown>;
+  readonly onChange: (callback: EventCallback) => void;
+  readonly onError: (callback: EventCallback) => void;
+  readonly request: (element?: HTMLElement) => Promise<unknown>;
+  readonly exit: () => Promise<unknown>;
+  readonly isFullscreen: boolean;
+  readonly element: HTMLElement | null;
+}
+
+export default function useFullscreen(): FullscreenApi;
