9e41aa4946a28030f348e4381ef19db1d9da7438
imbhargav5 <imbhargav5@gmail.com>
2020-10-30 00:40:04 +0530

    Fix git tracking for docusaurus

--- packages/docusaurus/docs-website/.docusaurus/client-modules.js
+++
@@ -1,8 +0,0 @@
-export default [
-  require("/Users/bhargavponnapalli/opensource/rooks/packages/docusaurus/docs-website/node_modules/remark-admonitions/styles/infima.css"),
-  require("/Users/bhargavponnapalli/opensource/rooks/packages/docusaurus/docs-website/node_modules/remark-admonitions/styles/infima.css"),
-  require("/Users/bhargavponnapalli/opensource/rooks/packages/docusaurus/docs-website/node_modules/remark-admonitions/styles/infima.css"),
-  require("/Users/bhargavponnapalli/opensource/rooks/packages/docusaurus/docs-website/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/bhargavponnapalli/opensource/rooks/packages/docusaurus/docs-website/node_modules/@docusaurus/theme-classic/src/prism-include-languages"),
-  require("/Users/bhargavponnapalli/opensource/rooks/packages/docusaurus/docs-website/src/css/customTheme.css"),
-];
