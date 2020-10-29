877d89edd81dc08efb85ef45fada80fde80066d1
imbhargav5 <imbhargav5@gmail.com>
2020-10-29 17:50:16 +0530

    Updates

--- .github/actions/publish-github-release/main.js
+++ .github/actions/publish-github-release/main.js
@@ -7,11 +7,16 @@ const github = require("@actions/github");
 const token = core.getInput("token", { required: true });
 const tag = core.getInput("tag", { required: true });
 const changelog = core.getInput("changelog", { required: true });
-const isTagStable = core.getInput("is-tag-stable", { required: true });
+const isTagStableInput = core.getInput("is-tag-stable", { required: true });
+const isTagStable = isTagStableInput === "1" || isTagStableInput === 1 ? true : false;
+const isPrerelease = !isTagStable;
+const shouldReleaseBeDraft = isTagStable;
 
 const [repoOwner, repoName] = process.env.GITHUB_REPOSITORY.split("/");
 
-console.log("isTagStable value is ", isTagStable)
+console.log("isTagStable value is ", isTagStable);
+console.log("shouldReleaseBeDraft value is ", shouldReleaseBeDraft);
+console.log("isPrerelease value is ", isPrerelease);
 
 const octokit = new github.GitHub(token);
 
@@ -20,7 +25,7 @@ octokit.repos.createRelease({
     repo: repoName,
     tag_name: tag,
     body: JSON.parse(changelog),
-    draft: true,
-    prerelease: false,
+    draft: shouldReleaseBeDraft,
+    prerelease: isPrerelease,
     name: tag,
 });
