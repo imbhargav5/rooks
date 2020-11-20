f490dd58565ec2578aae61cc539755a0a965eb5f
imbhargav5 <imbhargav5@gmail.com>
2020-11-20 12:13:27 +0530

    Get rid of storybook

--- packages/storybook/.storybook/addons.js
+++
@@ -1,6 +0,0 @@
-import registerWithPanelTitle from "storybook-readme/registerWithPanelTitle";
-import '@storybook/addon-storysource/register';
-registerWithPanelTitle("Docs");
-//import "@storybook/addon-actions/register";
-//import "@storybook/addon-knobs/register";
-//import "@storybook/addon-notes/register";
--- packages/storybook/.storybook/webpack.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = function({ config }) {
-  config.module.rules.push({
-    test: /\.js?$/,
-    loaders: [require.resolve("@storybook/addon-storysource/loader")],
-    enforce: "pre"
-  });
-
-  return config;
-};
