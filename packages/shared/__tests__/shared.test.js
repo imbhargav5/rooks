9efbb3fb849aa04c264d281c98ae8f2a4744beba
imbhargav5 <imbhargav5@gmail.com>
2020-11-20 22:35:06 +0530

    Init

--- packages/shared/__tests__/shared.test.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const shared = require('..');
-
-describe('shared', () => {
-    it('needs tests');
-});
---
+++ packages/shared/__tests__/useDebounce/setupTests.ts
@@ -0,0 +1,15 @@
+// this is just a little hack to silence a warning that we'll get until react
+// fixes this: https://github.com/facebook/react/pull/14853
+const originalError = console.error;
+beforeAll(() => {
+  console.error = (...args) => {
+    if (/Warning.*not wrapped in act/.test(args[0])) {
+      return;
+    }
+    originalError.call(console, ...args);
+  };
+});
+
+afterAll(() => {
+  console.error = originalError;
+});
