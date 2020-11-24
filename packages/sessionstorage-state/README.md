55b75f4ba40b5931cb46f3e1a1a208d36728ce27
imbhargav5 <imbhargav5@gmail.com>
2020-11-24 14:00:05 +0530

    Add session storage hook

---
+++ packages/sessionstorage-state/README.md
@@ -0,0 +1,34 @@
+# @rooks/use-sessionstorage-state
+
+[![Build Status](https://travis-ci.org/imbhargav5/rooks.svg?branch=master)](https://travis-ci.org/imbhargav5/rooks) ![](https://img.shields.io/npm/v/@rooks/use-sessionstorage-state/latest.svg) ![](https://img.shields.io/npm/l/@rooks/use-sessionstorage-state.svg) ![](https://img.shields.io/bundlephobia/min/@rooks/use-sessionstorage-state.svg) ![](https://img.shields.io/david/imbhargav5/rooks.svg?path=packages%2Fsessionstorage-state)
+
+
+
+## About
+useState but syncs with sessionstorage
+
+
+[//]: # (Main)
+
+## Installation
+
+```
+npm install --save @rooks/use-sessionstorage-state
+```
+
+## Importing the hook
+
+```javascript
+import useSessionstorageState from "@rooks/use-sessionstorage-state"
+```
+
+## Usage
+
+```jsx
+function Demo() {
+  useSessionstorageState();
+  return null
+}
+
+render(<Demo/>)
+```
---
+++ packages/shared/useSessionstorageState.ts
@@ -0,0 +1,88 @@
+import { useState, useEffect, useCallback, useRef, Dispatch, SetStateAction } from "react";
+
+function getValueFromSessionStorage(key) {
+  if (typeof sessionStorage === "undefined") {
+    return null;
+  }
+  const storedValue = sessionStorage.getItem(key) || "null";
+  try {
+    return JSON.parse(storedValue);
+  } catch (err) {
+    console.error(err);
+  }
+  return storedValue;
+}
+
+function saveValueToSessionStorage(key: string, value: any) {
+  if (typeof sessionStorage === "undefined") {
+    return null;
+  }
+  return sessionStorage.setItem(key, JSON.stringify(value));
+}
+
+
+/**
+ * @param key Key of the sessionStorage object
+ * @param initialState Default initial value
+ */
+function initialize(key, initialState){
+  const valueLoadedFromSessionStorage  = getValueFromSessionStorage(key)
+  if(valueLoadedFromSessionStorage === null){
+    return initialState
+  }else{
+    return valueLoadedFromSessionStorage
+  }
+}
+
+/**
+ * useSessionstorageState hook
+ * Tracks a value within sessionStorage and updates it
+ * @param {string} key - Key of the sessionStorage object
+ * @param {any} initialState - Default initial value
+ */
+function useSessionstorageState<S>(key: string, initialState?: S | (() => S)): [S, Dispatch<SetStateAction<S>>, () => void] {
+  const [value, __setValue] = useState(() => initialize(key, initialState));
+  const isUpdateFromListener = useRef(false);
+  
+  useEffect(() => {
+    /** 
+     * We need to ensure there is no loop of 
+     * storage events fired. Hence we are using a ref 
+     * to keep track of whether setValue is from another 
+     * storage event
+    */
+    if(!isUpdateFromListener.current){
+      saveValueToSessionStorage(key, value);
+    }
+  }, [value])
+
+
+  const listen = useCallback((e: StorageEvent) => {
+    if (e.storageArea === sessionStorage && e.key === key) {
+      isUpdateFromListener.current = true;
+      __setValue(e.newValue);
+    }
+  }, []);
+  
+  // check for changes across windows
+  useEffect(() => {
+    window.addEventListener("storage", listen);
+    return () => {
+      window.removeEventListener("storage", listen);
+    };
+  }, []);
+
+  
+  function setValue(newValue:S):void{
+    isUpdateFromListener.current = false;
+    __setValue(newValue)
+  }
+
+  function remove() {    
+    sessionStorage.removeItem(key);
+  }
+  
+  return [value, setValue, remove];
+}
+
+export {useSessionstorageState};
