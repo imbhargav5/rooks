58c581fa313620b888ef85905a9d875bd4eff85c
imbhargav5 <imbhargav5@gmail.com>
2020-11-24 13:57:01 +0530

    Add useLocalstorageState

---
+++ packages/shared/__tests__/useLocalstorageState.spec.tsx
@@ -0,0 +1,67 @@
+/**
+ * @jest-environment jsdom
+ */
+import React from "react";
+import {useLocalstorageState} from "../useLocalstorageState";
+import { render, cleanup, getByTestId, fireEvent, act } from "@testing-library/react";
+
+describe("useLocalstorageState defined", () => {
+  it("should be defined", () => {
+    expect(useLocalstorageState).toBeDefined();
+  });
+});
+
+
+describe("useLocalstorageState basic", () => {
+  let App;
+  beforeEach(() => {
+    // firstCallback = jest.fn()
+    App = function () {
+      const [value, set, remove] = useLocalstorageState("test-value", "hello");
+
+      return (
+        <div data-testid="container">
+          <p data-testid="value">{value}</p>
+          <button data-testid="new-value"
+            onClick={() => {
+              set("new value")
+            }}
+          >Set to new value</button>
+          <button data-testid="unset-value"
+            onClick={remove}
+          >Unset the value</button>
+        </div>
+      );
+    };
+    //end
+  });
+
+  afterEach(cleanup);
+
+  it("it initializes correctly", () => {
+    const { container } = render(<App />);
+    const valueElement = getByTestId(container as HTMLElement, "value");
+    expect(valueElement.innerHTML).toBe("hello");
+  });
+
+  it("setting the new value ", () => {
+    const { container } = render(<App />);
+    const setToNewValueBtn = getByTestId(container as HTMLElement, "new-value");
+    act(() => {
+      fireEvent.click(setToNewValueBtn)
+    })
+    const valueElement = getByTestId(container as HTMLElement, "value");
+    expect(valueElement.innerHTML).toBe("new value");
+  })
+
+  it.skip("unsetting the value", () => {
+    const { container } = render(<App />);
+    const unsetValueBtn = getByTestId(container as HTMLElement, "unset-value");
+    act(() => {
+      fireEvent.click(unsetValueBtn)
+    })
+    const valueElement = getByTestId(container as HTMLElement, "value");
+    expect(valueElement.innerHTML).toBe("");
+  })
+});
+
