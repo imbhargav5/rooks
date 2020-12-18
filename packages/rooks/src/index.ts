2e5b831945aab1aca4e856aaf53bce767daa077c
Bhargav Ponnapalli <imbhargav5@gmail.com>
2020-12-18 20:02:08 +0530

    Create 3 hooks (#329)
    * Add use-isomorphic-effect
    
    * Add fresh ref
    
    * Add test fresh-tick

--- packages/rooks/src/index.ts
+++ packages/rooks/src/index.ts
@@ -6,6 +6,8 @@ export { useDebounce } from "shared/useDebounce";
 export { useDidMount } from "shared/useDidMount";
 export { useDidUpdate } from "shared/useDidUpdate";
 export { useEffectOnceWhen } from "shared/useEffectOnceWhen";
+export { useFreshRef } from "shared/useFreshRef";
+export { useFreshTick } from "shared/useFreshTick";
 export { useFullscreen } from "shared/useFullscreen";
 export { useForkRef } from "shared/useForkRef";
 export { useGeolocation } from "shared/useGeolocation";
