93ed9f64b77580e2a90ad1f3566d281b360ca85b
Bhargav Ponnapalli <imbhargav5@gmail.com>
2021-01-28 08:02:38 +0530

    Optionally use layout effects (#360)
    * Remove unnecesary render
    
    * Optionally use isomorphic layout

--- packages/shared/useOnWindowScroll.ts
+++ packages/shared/useOnWindowScroll.ts
@@ -1,4 +1,5 @@
 import { useEffect, useRef } from "react";
+import { useIsomorphicEffect } from "./useIsomorphicEffect";
 
 /**
  *
@@ -6,15 +7,17 @@ import { useEffect, useRef } from "react";
  * Fires a callback when window scroll
  * @param {function} callback Callback to be called before unmount
  * @param {boolean} when When the handler should be applied
+ * @param {boolean} isLayoutEffect Should it use layout effect. Defaults to false
  */
-function useOnWindowScroll(callback: (event: any)=>void, when:boolean = true): void {
+function useOnWindowScroll(callback: (event: any)=>void, when:boolean = true, isLayoutEffect: boolean = false): void {
   const savedHandler = useRef(callback);
+  const useEffectToRun = isLayoutEffect ? useIsomorphicEffect : useEffect
 
-  useEffect(() => {
+  useEffectToRun(() => {
     savedHandler.current = callback;    
   })
 
-  useEffect(() => {
+  useEffectToRun(() => {
     if (when) {
       function passedCb(event) {
         savedHandler.current(event);
