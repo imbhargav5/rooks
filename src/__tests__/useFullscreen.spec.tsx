3f7b4b908fe3986bd886ee2605ac704c5c4f9e16
Nghia Pham <phmngocnghia@gmail.com>
2021-07-17 13:29:41 +0700

    fix: remove any, update tests

--- src/__tests__/useFullscreen.spec.tsx
+++ src/__tests__/useFullscreen.spec.tsx
@@ -2,20 +2,14 @@ import { renderHook, act } from "@testing-library/react-hooks";
 import { useFullscreen } from "../hooks/useFullscreen";
 
 describe("useFullscreen", () => {
-  it("should be defined", () => {
-    expect(useFullscreen).toBeDefined();
-  });
-  it("should return an object", () => {
-    const { result } = renderHook(() => useFullscreen());
-    expect(typeof result.current).toBe("object");
-  });
-  it.only("should forward requestFullscreenOptions to requestFullscreen", () => {
-    (document as any).exitFullscreen = jest.fn();
+  it("should forward requestFullscreenOptions to requestFullscreen", () => {
+    document.exitFullscreen = jest.fn();
     const element = { requestFullscreen: jest.fn() };
     const mockRequestFullscreenOptions = { navigationUI: "show" };
     const { result } = renderHook(() =>
       useFullscreen({ requestFullscreenOptions: { navigationUI: "show" } })
     );
+    expect(typeof result.current).toBe("object");
     act(() => {
       result.current?.request(element as any);
     });
