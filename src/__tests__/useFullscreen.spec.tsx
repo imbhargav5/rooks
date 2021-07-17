8d39d0fba31961446f960634dacb9afbe86a2eb7
Nghia Pham <phmngocnghia@gmail.com>
2021-07-17 11:48:16 +0700

    feat(usefullscreen): pass requestFullscreenOptions
    fix #505

--- src/__tests__/useFullscreen.spec.tsx
+++ src/__tests__/useFullscreen.spec.tsx
@@ -1,12 +1,26 @@
-import { renderHook } from '@testing-library/react-hooks';
-import { useFullscreen } from '../hooks/useFullscreen';
+import { renderHook, act } from "@testing-library/react-hooks";
+import { useFullscreen } from "../hooks/useFullscreen";
 
-describe('useFullscreen', () => {
-  it('should be defined', () => {
+describe("useFullscreen", () => {
+  it("should be defined", () => {
     expect(useFullscreen).toBeDefined();
   });
-  it('should return an object', () => {
+  it("should return an object", () => {
     const { result } = renderHook(() => useFullscreen());
-    expect(typeof result.current).toBe('object');
+    expect(typeof result.current).toBe("object");
+  });
+  it.only("should forward requestFullscreenOptions to requestFullscreen", () => {
+    (document as any).exitFullscreen = jest.fn();
+    const element = { requestFullscreen: jest.fn() };
+    const mockRequestFullscreenOptions = { navigationUI: "show" };
+    const { result } = renderHook(() =>
+      useFullscreen({ requestFullscreenOptions: { navigationUI: "show" } })
+    );
+    act(() => {
+      result.current?.request(element as any);
+    });
+    expect(element.requestFullscreen).toHaveBeenCalledWith(
+      mockRequestFullscreenOptions
+    );
   });
 });
