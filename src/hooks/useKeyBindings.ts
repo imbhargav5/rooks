c8a9fdcaf5555d204362d0fa095259023d4576b3
qiweiii <yangqiwei97@gmail.com>
2021-07-24 23:09:05 +0800

    fix: update usekeybindings hook

--- src/hooks/useKeyBindings.ts
+++ src/hooks/useKeyBindings.ts
@@ -1,28 +1,36 @@
-/* eslint-disable react-hooks/rules-of-hooks */
-/* eslint-disable guard-for-in */
 import { useKey } from './useKey';
+import type { Ref } from 'react';
 
 type Options = {
   /**
    * Condition which if true, will enable the event listeners
    */
   when?: boolean;
+  /**
+   * Keyboardevent types to listen for. Valid options are keyDown, keyPress and keyUp
+   */
+  eventTypes?: Array<number | string>;
+  /**
+   * target ref on which the events should be listened. If no target is specified,
+   * events are listened to on the window
+   */
+  target?: Ref<HTMLElement>;
 };
 
 /**
  * useKeyBindings
  *
- * useKeyBindings can bind multiple keys to multiple callbacks and fire the callbacks on key press.
+ * useKeyBindings binds pairs of keyboard events and handlers
  *
  * @param { [key: string]: () => void } keys
  * @param {Options} options
  */
 const useKeyBindings = (
-  keys: { [key: string]: () => void },
+  keyBindings: { [key: string]: () => void },
   options_?: Options
 ) => {
-  for (const key in keys) {
-    useKey(key, keys[key], options_);
+  for (const key in keyBindings) {
+    useKey(key, keyBindings[key], options_);
   }
 };
 
