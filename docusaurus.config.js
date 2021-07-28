ae7659af68d83f6043c79a11ff7bb256e12d026a
Qiwei Yang <32790369+qiweiii@users.noreply.github.com>
2021-07-28 11:19:21 -0700

    docs: add redirects for old links (#527)

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,95 +1,109 @@
 module.exports = {
-  title: 'Rooks',
-  tagline: 'Collection of React hooks ⚓ for everyone. ',
-  url: 'https://react-hooks.org',
-  baseUrl: '/',
-  organizationName: 'imbhargav5',
-  projectName: 'rooks',
+  title: "Rooks",
+  tagline: "Collection of React hooks ⚓ for everyone. ",
+  url: "https://react-hooks.org",
+  baseUrl: "/",
+  organizationName: "imbhargav5",
+  projectName: "rooks",
   scripts: [
     {
-      src: 'https://buttons.github.io/buttons.js',
+      src: "https://buttons.github.io/buttons.js",
       async: true,
       defer: true,
     },
     {
-      src: 'https://cdn.splitbee.io/sb.js',
+      src: "https://cdn.splitbee.io/sb.js",
       async: true,
     },
     {
-      src: 'https://cdn.jsdelivr.net/npm/@widgetbot/crate@3',
+      src: "https://cdn.jsdelivr.net/npm/@widgetbot/crate@3",
       async: true,
       defer: true,
     },
   ],
   favicon:
-    'https://github.com/imbhargav5/rooks/raw/master/.github/assets/favicon-24x24.png',
+    "https://github.com/imbhargav5/rooks/raw/master/.github/assets/favicon-24x24.png",
   customFields: {
     users: [
       {
-        caption: 'User1',
-        image: '/img/undraw_open_source.svg',
-        infoLink: 'https://www.facebook.com',
+        caption: "User1",
+        image: "/img/undraw_open_source.svg",
+        infoLink: "https://www.facebook.com",
         pinned: true,
       },
     ],
-    repoUrl: 'https://github.com/imbhargav5/rooks',
+    repoUrl: "https://github.com/imbhargav5/rooks",
   },
-  onBrokenLinks: 'log',
+  onBrokenLinks: "log",
   presets: [
     [
-      '@docusaurus/preset-classic',
+      "@docusaurus/preset-classic",
       {
         docs: {
           showLastUpdateAuthor: true,
           showLastUpdateTime: true,
-          path: 'docs',
-          sidebarPath: require.resolve('./sidebars.json'),
+          path: "docs",
+          sidebarPath: require.resolve("./sidebars.json"),
         },
         blog: {
-          path: 'blog',
+          path: "blog",
         },
         theme: {
-          customCss: [require.resolve('./src/css/customTheme.css')],
+          customCss: [require.resolve("./src/css/customTheme.css")],
         },
       },
     ],
   ],
-  plugins: [],
+  plugins: [
+    [
+      "@docusaurus/plugin-client-redirects",
+      {
+        createRedirects: function (existingPath) {
+          if (/use[A-Z]/.test(existingPath)) {
+            return existingPath
+              .split(/(?=[A-Z])/)
+              .join("-")
+              .toLowerCase();
+          }
+        },
+      },
+    ],
+  ],
   themeConfig: {
     navbar: {
-      title: 'Rooks',
+      title: "Rooks",
       logo: {
-        alt: 'Site Logo',
+        alt: "Site Logo",
         src:
-          'https://github.com/imbhargav5/rooks/raw/master/.github/assets/icon.png',
-        href: '/', // Default to `siteConfig.baseUrl`.
-        target: '_self', // By default, this value is calculated based on the `href` attribute (the external link will open in a new tab, all others in the current one).
+          "https://github.com/imbhargav5/rooks/raw/master/.github/assets/icon.png",
+        href: "/", // Default to `siteConfig.baseUrl`.
+        target: "_self", // By default, this value is calculated based on the `href` attribute (the external link will open in a new tab, all others in the current one).
       },
       items: [
         {
-          to: '/docs/getting-started/',
-          label: 'Getting Started',
-          position: 'left',
+          to: "/docs/getting-started/",
+          label: "Getting Started",
+          position: "left",
         },
         {
-          to: '/docs/',
-          label: 'Documentation',
-          position: 'left',
+          to: "/docs/",
+          label: "Documentation",
+          position: "left",
         },
         {
-          href: 'https://github.com/imbhargav5/rooks',
-          label: 'Github',
-          position: 'right',
+          href: "https://github.com/imbhargav5/rooks",
+          label: "Github",
+          position: "right",
         },
         {
-          href: '/docs/hooks-list/',
-          label: 'Hooks List',
-          position: 'left',
+          href: "/docs/hooks-list/",
+          label: "Hooks List",
+          position: "left",
         },
         {
-          href: '/docs/motivation/',
-          label: 'Motivation',
-          position: 'right',
+          href: "/docs/motivation/",
+          label: "Motivation",
+          position: "right",
         },
         // {
         //   "to": "/help",
@@ -97,60 +111,64 @@ module.exports = {
         //   "position": "left"
         // },
         {
-          type: 'docsVersionDropdown',
-          position: 'right',
+          type: "docsVersionDropdown",
+          position: "right",
           dropdownItemsBefore: [],
-          dropdownItemsAfter: [{ to: '/versions', label: 'All versions' }],
+          dropdownItemsAfter: [{ to: "/versions", label: "All versions" }],
         },
       ],
     },
-    image: 'img/undraw_online.svg',
+    image: "img/undraw_online.svg",
     footer: {
       links: [
         {
           // Label of the section of these links
-          title: 'Docs',
+          title: "Docs",
           items: [
             {
               // Label of the link
-              label: 'All in one',
+              label: "All in one",
               // Client-side routing, used for navigating within the website.
               // The baseUrl will be automatically prepended to this value.
-              to: 'docs/',
+              to: "docs/",
             },
             {
-              label: 'use-counter',
-              to: 'docs/use-counter/',
+              label: "Getting Started",
+              to: "docs/getting-started/",
             },
             {
-              label: 'use-did-mount',
-              to: 'docs/use-did-mount/',
+              label: "Motivation",
+              to: "docs/motivation/",
+            },
+            {
+              label: "Hooks List",
+              to: "docs/hooks-list/",
             },
           ],
         },
         {
           // Label of the section of these links
-          title: 'Community',
+          title: "Community",
           items: [
             {
               // Label of the link
-              label: 'Stack overflow',
+              label: "Stack overflow",
               // Client-side routing, used for navigating within the website.
               // The baseUrl will be automatically prepended to this value.
-              to: 'https://stackoverflow.com/questions/tagged/rooks',
+              to: "https://stackoverflow.com/questions/tagged/rooks",
             },
             {
-              label: 'Twitter',
-              to: 'https://twitter.com/imbhargav5',
+              label: "Twitter",
+              to: "https://twitter.com/imbhargav5",
             },
           ],
         },
         {
-          title: 'More',
+          title: "More",
           items: [
             {
-              label: 'Github',
-              to: 'https://github.com/imbhargav5/rooks',
+              label: "Github",
+              to: "https://github.com/imbhargav5/rooks",
             },
             //           {
             //             html: `
@@ -173,12 +191,12 @@ module.exports = {
           ],
         },
       ],
-      copyright: 'MIT© 2020 Bhargav Ponnapalli',
+      copyright: "MIT© 2020 Bhargav Ponnapalli",
       logo: {},
     },
     algolia: {
       apiKey: process.env.ALGOLIA_API_KEY,
-      indexName: 'react-hooks',
+      indexName: "react-hooks",
       // Optional: see doc section bellow,
       contextual: true,
       // searchParameters: {
