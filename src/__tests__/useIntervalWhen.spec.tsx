c13f8ef09a2bdfa02faf4d22ce5f597dd44978b3
Marius Oancea <marius.oancea@seeq.com>
2021-08-12 22:46:55 +0300

    add eager possibility for useIntervalWhen

--- src/__tests__/useIntervalWhen.spec.tsx
+++ src/__tests__/useIntervalWhen.spec.tsx
@@ -1,25 +1,31 @@
 /**
  * @jest-environment jsdom
  */
-import { renderHook, cleanup } from '@testing-library/react-hooks';
-import { useState } from 'react';
-import TestRenderer from 'react-test-renderer';
-import { useIntervalWhen } from '../hooks/useIntervalWhen';
+import { renderHook, cleanup } from "@testing-library/react-hooks";
+import { useState } from "react";
+import TestRenderer from "react-test-renderer";
+import { useIntervalWhen } from "../hooks/useIntervalWhen";
 
 const { act } = TestRenderer;
 
-describe('useIntervalWhen', () => {
+describe("useIntervalWhen", () => {
   let useHook;
+  const EAGER = true;
 
   beforeEach(() => {
-    useHook = function () {
+    useHook = function (when, eager = false) {
       const [currentValue, setCurrentValue] = useState(0);
       function increment() {
         setCurrentValue(currentValue + 1);
       }
-      useIntervalWhen(() => {
-        increment();
-      }, 1_000);
+      useIntervalWhen(
+        () => {
+          increment();
+        },
+        1_000,
+        when,
+        eager
+      );
 
       return { currentValue };
     };
@@ -30,12 +36,12 @@ describe('useIntervalWhen', () => {
     jest.clearAllTimers();
   });
 
-  it('should be defined', () => {
+  it("should be defined", () => {
     expect(useIntervalWhen).toBeDefined();
   });
-  it('should start timer when started with start function', () => {
+  it("should start timer when started with start function", () => {
     jest.useFakeTimers();
-    const { result } = renderHook(() => useHook());
+    const { result } = renderHook(() => useHook(true));
     act(() => {
       jest.advanceTimersByTime(1_000);
     });
@@ -44,14 +50,17 @@ describe('useIntervalWhen', () => {
     jest.useRealTimers();
   });
 
-  it('should start timer when started with start function in array destructuring', () => {
+  it("should call the callback eagerly", () => {
     jest.useFakeTimers();
-    const { result } = renderHook(() => useHook());
+    const { result } = renderHook(() => useHook(true, EAGER));
+    // The value was already incremented because we use useIntervalWhen in EAGER mode
+    expect(result.current.currentValue).toBe(1);
     act(() => {
       jest.advanceTimersByTime(1_000);
     });
     expect(setInterval).toHaveBeenCalledTimes(1);
-    expect(result.current.currentValue).toBe(1);
+    // The value was incremented twice: one time by the setInterval and one time due to the EAGER
+    expect(result.current.currentValue).toBe(2);
     jest.useRealTimers();
   });
 });
