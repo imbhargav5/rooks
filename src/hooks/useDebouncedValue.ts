92b3cb5849dde9da81b5f12df6a0f3777e7bf07a
maciek_grzybek <maciekgrzybek1@gmail.com>
2021-08-14 13:23:06 +0100

    feat(useDebouncedValue): add useDebouncedValue hook (#552)

---
+++ src/hooks/useDebouncedValue.ts
@@ -0,0 +1,32 @@
+import { SetStateAction, Dispatch, useState } from "react";
+import {useDebounce} from './useDebounce'
+import { useDidMount } from "./useDidMount";
+import { useDidUpdate } from "./useDidUpdate";
+
+type UseDebouncedValueOptions = Partial<{
+  initializeWithNull: boolean;
+}>
+
+const defaultUseDebounceValueOptions = {
+  initializeWithNull: false
+}
+
+type UseDebouncedValueReturnType <T> = [debouncedValue: T | null, immediatelyUpdateDebouncedValue : Dispatch<SetStateAction<T| null>>]
+
+export const useDebouncedValue = <T = unknown>(value: T, timeout: number, options: UseDebouncedValueOptions = {}) : UseDebouncedValueReturnType<T> => {
+  const {initializeWithNull} = Object.assign({}, defaultUseDebounceValueOptions,options)
+  const [updatedValue, setUpdatedValue] = useState<T | null>( initializeWithNull ? null : value);
+  const debouncedSetUpdatedValue = useDebounce(setUpdatedValue, timeout);  
+  useDidMount(() => {
+    if(initializeWithNull){
+      debouncedSetUpdatedValue(value);
+    }
+  })
+  useDidUpdate(() => {
+    debouncedSetUpdatedValue(value);
+  }, [value])
+  
+  // No need to add `debouncedSetUpdatedValue ` to dependencies as it is a ref.current.
+  // returning both updatedValue and setUpdatedValue (not the debounced version) to instantly update this if  needed. 
+  return [updatedValue , setUpdatedValue];
+}
