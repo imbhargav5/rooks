13ebe9263d67bed2f51fc9900568cb87bf63b457
mar1u50 <mar1u50@yahoo.ca>
2021-08-18 09:38:36 +0300

    feat(useIntervalWhen): add support to fire the first callback eagerly
    * add eager possibility for useIntervalWhen
    
    * docs(useIntervalWhen): add examples and new argument
    
    Co-authored-by: Marius Oancea <marius.oancea@seeq.com>
    Co-authored-by: qiweiii <yangqiwei97@gmail.com>

--- src/hooks/useIntervalWhen.ts
+++ src/hooks/useIntervalWhen.ts
@@ -1,4 +1,4 @@
-import { useRef, useEffect } from 'react';
+import { useRef, useEffect } from "react";
 
 /**
  * A setInterval hook that calls a callback after a interval duration
@@ -7,11 +7,13 @@ import { useRef, useEffect } from 'react';
  * @param cb The callback to be invoked after interval
  * @param intervalDurationMs Amount of time in ms after which to invoke
  * @param when The condition which when true, sets the interval
+ * @param startImmediate If the callback should be invoked immediately
  */
 function useIntervalWhen(
   callback_: () => void,
   intervalDurationMs: number = 0,
-  when: boolean = true
+  when: boolean = true,
+  startImmediate: boolean = false
 ): void {
   const savedRefCallback = useRef<() => any>();
 
@@ -25,6 +27,9 @@ function useIntervalWhen(
 
   useEffect(() => {
     if (when) {
+      if (startImmediate) {
+        callback();
+      }
       const interval = window.setInterval(callback, intervalDurationMs);
 
       return () => {
