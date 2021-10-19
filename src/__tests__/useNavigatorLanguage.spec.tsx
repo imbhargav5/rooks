e91873379539c525e654ab5a36614d352561326f
qiweiii <yangqiwei97@gmail.com>
2021-10-19 23:06:30 +0800

    tests: useOnline and useNavigator

--- src/__tests__/useNavigatorLanguage.spec.tsx
+++ src/__tests__/useNavigatorLanguage.spec.tsx
@@ -5,12 +5,19 @@ import { renderHook } from "@testing-library/react-hooks";
 import { useNavigatorLanguage } from "../hooks/useNavigatorLanguage";
 
 describe("useNavigatorLanguage", () => {
+  let languageGetter;
+
+  beforeEach(() => {
+    languageGetter = jest.spyOn(window.navigator, "language", "get");
+  });
+
   it("should be defined", () => {
     expect(useNavigatorLanguage).toBeDefined();
   });
 
   it("should get the navigator language", () => {
+    languageGetter.mockReturnValue("de");
     const { result } = renderHook(() => useNavigatorLanguage());
-    expect(result.current).toBe("en-US");
+    expect(result.current).toBe("de");
   });
 });
--- src/__tests__/useOnline.spec.tsx
+++ src/__tests__/useOnline.spec.tsx
@@ -5,12 +5,25 @@ import { renderHook } from "@testing-library/react-hooks";
 import { useOnline } from "../hooks/useOnline";
 
 describe("useOnline", () => {
+  let onlineGetter;
+
+  beforeEach(() => {
+    onlineGetter = jest.spyOn(window.navigator, "onLine", "get");
+  });
+
   it("should be defined", () => {
     expect(useOnline).toBeDefined();
   });
 
   it("should get the online status", () => {
+    onlineGetter.mockReturnValue(true);
     const { result } = renderHook(() => useOnline());
     expect(result.current).toBe(true);
+  });
+
+  it("should get the offline status", () => {
+    onlineGetter.mockReturnValue(false);
+    const { result } = renderHook(() => useOnline());
+    expect(result.current).toBe(false);
   });
 });
