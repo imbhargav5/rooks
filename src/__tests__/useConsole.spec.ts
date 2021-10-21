d4fe3d6534aee423410a4a7ab22aac2cc039fe6e
Ante Barić <ante.baric@burza.hr>
2021-10-21 23:33:10 +0200

    feat: add tests

--- src/__tests__/useConsole.spec.ts
+++ src/__tests__/useConsole.spec.ts
@@ -1,13 +1,72 @@
 /**
  * @jest-environment jsdom
  */
-import React from "react";
+import { cleanup } from "@testing-library/react";
+import { renderHook } from "@testing-library/react-hooks";
+import { useState } from "react";
+import TestRenderer from "react-test-renderer";
 import { useConsole } from "../hooks/useConsole";
 
+const { act } = TestRenderer;
+
+const useHook = () => {
+  const [value, setValue] = useState(0);
+
+  useConsole("info", value);
+
+  return { setValue, value };
+};
+
+const useInfoHook = () => {
+  const [value, setValue] = useState(0);
+
+  useConsole(value);
+
+  return { setValue, value };
+};
+
 describe("useConsole", () => {
+  afterEach(cleanup);
+
   it("should be defined", () => {
     expect(useConsole).toBeDefined();
   });
-});
 
-// figure out tests
+  it("should log to console when value changes", () => {
+    const consoleSpy = jest.spyOn(console, "log");
+    const { result } = renderHook(() => useInfoHook());
+
+    expect(consoleSpy).toHaveBeenCalledWith(0);
+
+    act(() => {
+      result.current.setValue(1);
+    });
+
+    expect(consoleSpy).toHaveBeenCalledWith(1);
+
+    act(() => {
+      result.current.setValue(1);
+    });
+
+    expect(consoleSpy).toHaveBeenCalledTimes(2);
+  });
+
+  it("should use different underlying console method when level is provided", () => {
+    const consoleSpy = jest.spyOn(console, "info");
+    const { result } = renderHook(() => useHook());
+
+    expect(consoleSpy).toHaveBeenCalledWith(0);
+
+    act(() => {
+      result.current.setValue(1);
+    });
+
+    expect(consoleSpy).toHaveBeenCalledWith(1);
+
+    act(() => {
+      result.current.setValue(1);
+    });
+
+    expect(consoleSpy).toHaveBeenCalledTimes(2);
+  });
+});
