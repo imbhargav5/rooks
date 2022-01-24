9acb2e9a8f75f4a1b9de361c3bdf9e1b7ffb1608
mrdulin <novaline.dulin@gmail.com>
2022-01-24 15:51:49 +0800

    feat: improved unit tests for useDebounce, moved JSDoc types to typescript types, removed unnecessary for useEffect

--- src/hooks/useDebounce.ts
+++ src/hooks/useDebounce.ts
@@ -1,4 +1,4 @@
-import type { DebouncedFunc } from "lodash";
+import type { DebouncedFunc, DebounceSettings } from "lodash";
 import debounce from "lodash.debounce";
 import { useRef, useEffect, useCallback } from "react";
 
@@ -6,15 +6,19 @@ import { useRef, useEffect, useCallback } from "react";
  * Debounce hook
  * Debounces a function
  *
- * @param {Function} callback The callback to debounce
- * @param {number} wait The duration to debounce
- * @returns {Function} The debounced callback
+ * @param callback The callback to debounce
+ * @param wait The duration to debounce
+ * @param options The options object.
+ * @param options.leading Specify invoking on the leading edge of the timeout.
+ * @param options.maxWait The maximum time func is allowed to be delayed before it’s invoked.
+ * @param options.trailing Specify invoking on the trailing edge of the timeout.
+ * @returns Returns the new debounced function.
  */
 function useDebounce<T extends (...args: any[]) => unknown>(
   callback: T,
-  wait: number,
-  options?: {}
-): DebouncedFunc<T> {
+  wait?: number,
+  options?: DebounceSettings
+) {
   const createDebouncedCallback = useCallback(
     (function_: T): DebouncedFunc<T> => {
       return debounce(function_, wait, options);
@@ -22,18 +26,13 @@ function useDebounce<T extends (...args: any[]) => unknown>(
     [wait, options]
   );
 
-  const callbackRef = useRef<T>(callback);
   const debouncedCallbackRef = useRef<DebouncedFunc<T>>(
     createDebouncedCallback(callback)
   );
 
   useEffect(() => {
-    callbackRef.current = callback;
-  });
-
-  useEffect(() => {
-    debouncedCallbackRef.current = createDebouncedCallback(callbackRef.current);
-  }, [wait, options, createDebouncedCallback]);
+    debouncedCallbackRef.current = createDebouncedCallback(callback);
+  }, [callback, createDebouncedCallback]);
 
   return debouncedCallbackRef.current;
 }
