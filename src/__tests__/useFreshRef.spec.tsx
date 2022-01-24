53fd24464054823a1038045213cc1d7aba0340a7
official_dulin <novaline.dulin@gmail.com>
2022-01-24 21:57:18 +0800

    feat(useFreshRef): add more unit tests for useFreshRef and fix types (#804)

--- src/__tests__/useFreshRef.spec.tsx
+++ src/__tests__/useFreshRef.spec.tsx
@@ -1,15 +1,12 @@
 /**
  * @jest-environment jsdom
  */
-import { renderHook, cleanup } from "@testing-library/react-hooks";
-import { useEffect, useState } from "react";
-import TestRenderer from "react-test-renderer";
+import { renderHook, cleanup, act } from "@testing-library/react-hooks";
+import { useCallback, useEffect, useState } from "react";
 import { useFreshRef } from "../hooks/useFreshRef";
 
-const { act } = TestRenderer;
-
 describe("useFreshRef", () => {
-  let useHook;
+  let useHook: () => { currentValue: number };
 
   beforeEach(() => {
     useHook = function () {
@@ -24,7 +21,8 @@ describe("useFreshRef", () => {
             freshIncrementRef.current();
           }
         }
-        const intervalId = setInterval(tick, 1000);
+        const intervalId = setInterval(tick, 1_000);
+
         return () => {
           clearInterval(intervalId);
         };
@@ -41,12 +39,89 @@ describe("useFreshRef", () => {
   });
   it("should increment correctly", () => {
     jest.useFakeTimers();
-    const { result } = renderHook(() => useHook());
+    const { result } = renderHook(useHook);
     act(() => {
       jest.advanceTimersByTime(5_000);
     });
     expect(setInterval).toHaveBeenCalledTimes(1);
     expect(result.current.currentValue).toBe(5);
     jest.useRealTimers();
+  });
+
+  test("should use stale state", async () => {
+    expect.hasAssertions();
+    const { result } = renderHook(() => {
+      const [count, setCount] = useState(0);
+      const increment = () => {
+        setCount(count + 1);
+      };
+
+      const handler = useCallback(() => {
+        increment();
+      }, []);
+
+      return {
+        count,
+        handler,
+      } as const;
+    });
+    const handlers: Array<() => void> = [];
+    act(() => {
+      result.current.handler();
+    });
+    handlers.push(result.current.handler);
+    expect(result.current.count).toEqual(1);
+    act(() => {
+      result.current.handler();
+    });
+    handlers.push(result.current.handler);
+    act(() => {
+      result.current.handler();
+    });
+    handlers.push(result.current.handler);
+    expect(result.current.count).toEqual(1);
+    expect(handlers.every((function_) => function_ === handlers[0])).toBe(true);
+  });
+
+  test("should use the latest state via fresh ref and return stable ref object won’t and won't change on re-renders", async () => {
+    expect.assertions(8);
+    const { result } = renderHook(() => {
+      const [count, setCount] = useState(0);
+      const increment = () => {
+        setCount(count + 1);
+      };
+      const ref = useFreshRef<typeof increment>(increment);
+
+      const handler = useCallback(() => {
+        ref.current();
+      }, [ref]);
+
+      return { count, handler, ref } as const;
+    });
+    const handlers: Array<() => void> = [];
+    const references: Array<typeof result.current.ref> = [];
+    act(() => {
+      result.current.handler();
+    });
+    handlers.push(result.current.handler);
+    references.push(result.current.ref);
+    expect(result.current.count).toEqual(1);
+    act(() => {
+      result.current.handler();
+    });
+    handlers.push(result.current.handler);
+    references.push(result.current.ref);
+    expect(result.current.count).toEqual(2);
+    act(() => {
+      result.current.handler();
+    });
+    handlers.push(result.current.handler);
+    references.push(result.current.ref);
+    handlers.forEach((function_) => {
+      expect(function_).toBe(handlers[0]);
+    });
+    references.forEach((ref) => {
+      expect(ref).toBe(references[0]);
+    });
   });
 });
