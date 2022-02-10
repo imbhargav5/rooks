78d72c4a016c2538d83e05371bcde7f1445add6a
official_dulin <novaline.dulin@gmail.com>
2022-02-11 07:12:44 +0800

    build(jest): replace code transformer babel-jest with esbuild-jest, make the jest tests run faster (#811)
    * feat: replace code transformer babel-jest with esbuild-jest, make the jest tests run faster
    
    * fix: TS type error when run test with --coverage flag

--- jest.config.ts
+++ jest.config.ts
@@ -1,12 +1,15 @@
 // jest.config.ts
-import type { Config } from '@jest/types';
+import type { Config } from "@jest/types";
 
 // Or async function
 export default async (): Promise<Config.InitialOptions> => {
   return {
-    preset: 'ts-jest',
-    testEnvironment: 'jsdom',
-    testMatch: ['<rootDir>/src/__tests__/**/*.(spec|test).(ts|tsx)'],
+    testEnvironment: "jsdom",
+    testMatch: ["<rootDir>/src/__tests__/**/*.(spec|test).(ts|tsx)"],
     verbose: true,
+    transform: {
+      "^.+\\.[jt]sx?$": "esbuild-jest",
+    },
+    coverageProvider: "v8",
   };
 };
