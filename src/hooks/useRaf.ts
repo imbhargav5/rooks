653cccdc3588f465dffd97d707bb8f487ab16aaf
iamriot <iamriot.dev@gmail.com>
2022-06-27 18:06:15 +0800

    fix useRaf resetting startTime on every tick

--- src/hooks/useRaf.ts
+++ src/hooks/useRaf.ts
@@ -25,7 +25,6 @@ export function useRaf(
 
     function tick() {
       const timeElapsed = Date.now() - startTime;
-      startTime = Date.now();
       loop();
       savedCallback.current && savedCallback.current(timeElapsed);
     }
