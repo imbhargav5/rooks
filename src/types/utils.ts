b39f69d967b250fb54354258c43ead2d1e81433e
Qiushi Pan <17402261+qqpann@users.noreply.github.com>
2022-06-30 21:19:51 +0900

    fix(useGlobalEventListener): Better typing for useGlobalEventListener (#1070)
    * chore: fix type check; format
    
    * chore: add types to listenerOptions
    
    * chore: add type to hooks that uses listenerOptions
    
    * chore: revert format for convergence
    
    * chore: fully type listenerOptions
    
    * chore: extract ListenerOptions type
    
    Co-authored-by: Qiushi Pan <17402261+qqhann@users.noreply.github.com>

--- src/types/utils.ts
+++ src/types/utils.ts
@@ -1,3 +1,12 @@
 export type DeepNullable<T> = {
   [K in keyof T]: DeepNullable<T[K]> | null;
 };
+
+export type ListenerOptions =
+  | boolean
+  | {
+      capture?: boolean;
+      once?: boolean;
+      passive?: boolean;
+      signal?: AbortSignal;
+    };
