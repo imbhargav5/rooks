de0107c21d1b1eb8a8e751d2e0ce4757bfbe751b
Bhargav Ponnapalli <imbhargav5@gmail.com>
2022-07-26 10:09:03 +0530

    fix: use async effect improvements

--- packages/rooks/src/hooks/useAsyncEffect.ts
+++ packages/rooks/src/hooks/useAsyncEffect.ts
@@ -1,35 +1,43 @@
-import React, { DependencyList, useEffect } from "react";
+import { type DependencyList, useEffect, useRef, useCallback } from "react";
+import { useGetIsMounted } from "./useGetIsMounted";
 
-type Effect = (signal: AbortSignal) => Promise<void | CleanupFunction>;
+type Effect = (signal: AbortSignal) => Promise<CleanupFunction | undefined>;
 type CleanupFunction = () => void;
 
 /**
  * A version of useEffect that accepts an async function
+ *
  * @param effect Async function that can return a cleanup function and takes in an AbortSignal
  * @param deps If present, effect will only activate if the values in the list change
  */
 function useAsyncEffect(effect: Effect, deps?: DependencyList) {
-  useEffect(() => {
-    const controller = new AbortController();
+  const lastCallId = useRef(0);
+  const getIsMounted = useGetIsMounted();
 
-    const effectPromise = effect(controller.signal).catch(() => {
-      console.error(
-        "You should NEVER throw inside useAsyncEffect. This means the cleanup function will not run, which can cause unintended side effects. Please wrap your useAsyncEffect function in a try/catch."
-      );
-    });
-
-    return () => {
-      controller.abort();
-      runCleanup();
-
-      async function runCleanup() {
-        const cleanupFunction = await effectPromise;
-        if (cleanupFunction) {
-          cleanupFunction();
+  const callback = useCallback(() => {
+    const callId = ++lastCallId.current;
+    const signal = new AbortSignal();
+    effect(signal)
+      .then((cleanup) => {
+        if (getIsMounted() && callId === lastCallId.current) {
+          signal.addEventListener("abort", () => {
+            if (callId === lastCallId.current) {
+              cleanup?.();
+            }
+          });
         }
-      }
-    };
-  }, deps);
+      })
+      .catch((error) => {
+        if (getIsMounted() && callId === lastCallId.current) {
+          throw error;
+        }
+      });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [effect, getIsMounted, deps]);
+
+  useEffect(() => {
+    callback();
+  }, [callback]);
 }
 
 export { useAsyncEffect };
