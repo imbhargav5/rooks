4c84179f16eb20cc43c0ad2e001d22642f5b6c13
G H Mahimaanvita <mahimagangavarapu@gmail.com>
2022-08-11 11:57:08 +0530

    use global.window

--- packages/rooks/src/hooks/useWindowEventListener.ts
+++ packages/rooks/src/hooks/useWindowEventListener.ts
@@ -19,20 +19,14 @@ function useWindowEventListener(
   listenerOptions: ListenerOptions = {},
   isLayoutEffect = false
 ): void {
-  if (typeof window !== "undefined") {
-    /*
-    Since the above condition changes values only across different environments, it is fine to call the hook conditionally
-    */
-    // eslint-disable-next-line react-hooks/rules-of-hooks
-    useGlobalObjectEventListener(
-      window,
-      eventName,
-      callback,
-      listenerOptions,
-      true,
-      isLayoutEffect
-    );
-  }
+  useGlobalObjectEventListener(
+    global.window,
+    eventName,
+    callback,
+    listenerOptions,
+    true,
+    isLayoutEffect
+  );
 }
 
 export { useWindowEventListener };
