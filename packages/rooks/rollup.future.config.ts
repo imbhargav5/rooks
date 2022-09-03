e805e1c560cd562d8bd2dfb278106b347b82b919
Sébastien Vanvelthem <belgattitude@gmail.com>
2022-09-03 14:46:36 +0200

    feat: add umd build, fix esm export

--- packages/rooks/rollup.future.config.ts
+++ packages/rooks/rollup.future.config.ts
@@ -1,14 +1,22 @@
 // import esbuild from "rollup-plugin-esbuild";
-// import terser from "rollup-plugin-terser";
 import dts from "rollup-plugin-dts";
 import typescript2 from "rollup-plugin-typescript2";
 import { typescriptPaths } from "rollup-plugin-typescript-paths";
 import pkg from "./package.json";
+import nodeResolve from "@rollup/plugin-node-resolve";
+import sourceMaps from "rollup-plugin-sourcemaps";
+import json from "@rollup/plugin-json";
+import babel from "@rollup/plugin-babel";
+import commonjs from "@rollup/plugin-commonjs";
+import replace from "@rollup/plugin-replace";
+import { terser } from "rollup-plugin-terser";
 
 const external = [
   ...Object.keys(pkg.dependencies),
   ...Object.keys(pkg.peerDependencies || {}),
 ];
+
+const umdGlobals = { react: "React", "react-dom": "ReactDOM" };
 
 const distDir = "./dist/future";
 
@@ -23,6 +31,29 @@ const plugins = [
     tsconfig: "./tsconfig.build.json",
   }),
 ];
+
+const umdPlugins = [
+  nodeResolve(),
+  typescript2({
+    tsconfig: "./tsconfig.build.json",
+  }),
+  sourceMaps(),
+  json(),
+  babel({
+    babelHelpers: "bundled",
+    exclude: ["node_modules/**", "../../node_modules/**"],
+    plugins: ["@babel/plugin-external-helpers"],
+  }),
+  commonjs({
+    ignoreGlobal: true,
+  }),
+  replace({
+    preventAssignment: true,
+    __VERSION__: JSON.stringify(pkg.version),
+  }),
+];
+
+const prodUmdPlugins = [terser()];
 
 export default () => [
   // ESM
@@ -41,7 +72,7 @@ export default () => [
   {
     input: ["./src/index.ts"],
     output: {
-      dir: `${distDir}/esm`,
+      dir: `${distDir}/cjs`,
       format: "cjs",
       sourcemap: false,
     },
@@ -65,4 +96,19 @@ export default () => [
     ],
   },
   // @todo UMD from rollup.config.ts
+  {
+    input: "./src/index.ts",
+    // \0 is rollup convention for generated in memory modules
+    external: Object.keys(umdGlobals),
+    plugins: umdPlugins,
+    output: [
+      {
+        file: `${distDir}/umd/rooks.umd.js`,
+        format: "umd",
+        globals: umdGlobals,
+        name: "rooks",
+        plugins: prodUmdPlugins,
+      },
+    ],
+  },
 ];
