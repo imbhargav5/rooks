99643ab77eee3997e2b02328a22279d0aabfc08d
Sébastien Vanvelthem <belgattitude@gmail.com>
2022-09-08 18:31:13 +0200

    chore(size-limit): improve output

--- packages/rooks/.size-limit.js
+++ packages/rooks/.size-limit.js
@@ -1,22 +1,41 @@
+// @ts-check
+
+const fullBundleMaxSize = "12KB";
+
+/**
+ * Will ensure esm tree-shakeability and total size are within expectations.
+ *
+ * @link https://github.com/ai/size-limit/
+ * @type {{name: string, path: string[], limit: string, import?: string, webpack?: boolean}[]}
+ */
 module.exports = [
+  // Size of all imports from the esm bundle
   {
-    name: "ESM Webpack (useDebounce)",
+    name: "ESM Webpack (*)",
+    webpack: true,
+    path: ["dist/esm/index.js"],
+    import: "*",
+    limit: fullBundleMaxSize,
+  },
+  // Size of an individual import from the esm bundle
+  {
+    name: "ESM Webpack ({ useDebounce })",
     webpack: true,
     path: ["dist/esm/index.js"],
     import: "{ useDebounce }",
     limit: "2KB",
   },
   {
-    name: "CJS Webpack (useDebounce)",
+    name: "CJS Webpack (*)",
     path: ["dist/cjs/index.js"],
     webpack: true,
-    import: "{ useDebounce }",
-    limit: "15KB",
+    import: "*", // CJS does not tree-shake in webpack5.
+    limit: fullBundleMaxSize,
   },
   {
-    name: "UMD bundle size",
+    name: "UMD bundle size (*)",
     path: ["dist/umd/rooks.umd.js"],
-    webpack: true,
-    limit: "10KB",
+    webpack: false,
+    limit: fullBundleMaxSize,
   },
 ];
