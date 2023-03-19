74366bc1339014025d14265954c4b8d2f64fde9f
Bhargav Ponnapalli <imbhargav5@gmail.com>
2023-03-19 23:32:31 +0530

    updates

---
+++ packages/rooks/src/__tests__/useSafeSetState.spec.ts
@@ -0,0 +1,56 @@
+import { renderHook, act } from "@testing-library/react-hooks";
+import useSafeSetState from "@/hooks/useSafeSetState";
+
+describe("useSafeSetState", () => {
+  it("should update state when the component is mounted", () => {
+    expect.hasAssertions();
+    const { result } = renderHook(() => useSafeSetState(0));
+    const [, safeSetState] = result.current;
+
+    act(() => {
+      safeSetState(1);
+    });
+
+    expect(result.current[0]).toBe(1);
+  });
+
+  it("should not update state when the component is unmounted", () => {
+    expect.hasAssertions();
+    const { result, unmount } = renderHook(() => useSafeSetState(0));
+    const [, safeSetState] = result.current;
+
+    unmount();
+
+    act(() => {
+      safeSetState(1);
+    });
+
+    expect(result.current[0]).toBe(0);
+  });
+
+  it("should handle functional updates correctly", () => {
+    expect.hasAssertions();
+    const { result } = renderHook(() => useSafeSetState(0));
+    const [, safeSetState] = result.current;
+
+    act(() => {
+      safeSetState((prevState) => prevState + 1);
+    });
+
+    expect(result.current[0]).toBe(1);
+  });
+
+  it("should not update state with functional updates when the component is unmounted", () => {
+    expect.hasAssertions();
+    const { result, unmount } = renderHook(() => useSafeSetState(0));
+    const [, safeSetState] = result.current;
+
+    unmount();
+
+    act(() => {
+      safeSetState((prevState) => prevState + 1);
+    });
+
+    expect(result.current[0]).toBe(0);
+  });
+});
---
+++ packages/rooks/src/hooks/useSafeSetState.ts
@@ -0,0 +1,25 @@
+/**
+ * useSafeSetState
+ * @description set state but ignores if component has already unmounted
+ * @see {@link https://rooks.vercel.app/docs/useSafeSetState}
+ */
+import { useState, useCallback, SetStateAction, Dispatch } from "react";
+import { useGetIsMounted } from "./useGetIsMounted";
+
+function useSafeSetState<T>(initialState: T): [T, Dispatch<SetStateAction<T>>] {
+  const [state, setState] = useState<T>(initialState);
+  const getIsMounted = useGetIsMounted();
+
+  const safeSetState = useCallback(
+    (newState: SetStateAction<T>) => {
+      if (getIsMounted()) {
+        setState(newState);
+      }
+    },
+    [getIsMounted]
+  );
+
+  return [state, safeSetState];
+}
+
+export default useSafeSetState;
