f93fbc28331b430f8b76dbb78c9371240fb1f68e
Bhargav Ponnapalli <imbhargav5@gmail.com>
2025-03-18 22:09:13 +0530

    feat: redirect to documentation on page load
    - Implemented a client-side redirect to the '/docs' route using Next.js's useRouter and useEffect hooks in the main page component.
    - This change enhances user experience by directing users to relevant documentation immediately upon loading the page.

--- apps/website/src/app/page.tsx
+++ apps/website/src/app/page.tsx
@@ -1,3 +1,12 @@
+'use client';
+
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
 export default function Page() {
+    const router = useRouter();
+    useEffect(() => {
+        router.replace('/docs');
+    }, []);
     return <div className="text-4xl font-bold">Hello World</div>;
 }
